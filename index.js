713376c73eb17e67e3ab236b498e34046bc0b424
gdsmith1 <gibsmith619@outlook.com>
2024-04-28 16:20:51 -0700

    practice demo commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
         message: "My name is Gibson",
         timestamp: new Date().getTime(),
         message2: "Hello world!",
-        message3: "Live testing..."
+        message3: "foobar"
     };
     res.send(JSON.stringify(data));
 });
